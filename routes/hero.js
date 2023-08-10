0c4cfd42070697d1d2355dcb34e95e1ca9917b37
fabert111 <119818844+fabert111@users.noreply.github.com>
2023-08-09 21:47:55 -0500

    Hero Router

--- routes/hero.js
+++ routes/hero.js
@@ -11,4 +11,4 @@ router.post('/create',controllerHero.create)
 router.patch('/update/:id',controllerHero.updateHero)
 router.delete('/delete/:id',controllerHero.deleteHero)
 
-module.exports = router
+module.exports = router 
